970c8bb4061bbb003e89f18ed9a9eaf6d544c988
0xSabdadev <jasonalhilal@gmail.com>
2023-01-01 05:12:33 +0700

    hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg'
 import './App.css'
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 function App() {
     return (
         <div className='home'>
             <Header />
+            <HeroSection />
         </div>
     )
 }
