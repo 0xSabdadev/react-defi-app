73fdf4df01b2cadf2da876c3261afe458142da94
0xSabdadev <jasonalhilal@gmail.com>
2023-01-01 12:00:00 +0700

    tokenCard && FeatureToken added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg'
 import './App.css'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
+import FeatureOne from './components/FeatureOne'
 
 function App() {
     return (
         <div className='home'>
             <Header />
             <HeroSection />
+            <FeatureOne />
         </div>
     )
 }
