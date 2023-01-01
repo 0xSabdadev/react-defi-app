a5aa4b7ee4bcd3e3bb32740cd27b72b64bed0d5c
0xSabdadev <jasonalhilal@gmail.com>
2023-01-01 14:35:49 +0700

    dailyStats added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import FeatureOne from './components/FeatureOne'
+import DailyStats from './components/DailyStats'
 
 function App() {
     return (
         <div className='home'>
             <Header />
             <HeroSection />
+            <DailyStats />
             <FeatureOne />
         </div>
     )
